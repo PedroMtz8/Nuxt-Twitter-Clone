72cb7934ae9648311102f120739b61b96aa8160d
Pedro Martinez <pedrocmartinez.dev@gmail.com>
2024-02-18 22:31:46 -0600

    left sidebar

--- app.vue
+++ app.vue
@@ -1,6 +1,33 @@
 <template>
-  <div>
-    <!-- <NuxtWelcome /> -->
-    <h1 class='text-green-500'>Hello world</h1>
+  <div :class="{ 'dark': darkMode }">
+
+    <div class='bg-white dark:bg-dim-900 '>
+
+      <div class='min-h-full'>
+        <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+
+          <!-- Left sidebar -->
+          <div class="hidden md:block xs-col-span-1 xl:col-span-2 ">
+            <div class="sticky top-0">
+              <SidebarLeft />
+            </div>
+          </div>
+
+           <!-- Main content -->
+           <main class="col-span-12 md:col-span-8 xl:col-span-6">
+             <router-view />
+           </main>
+
+        </div>
+      </div>
+
+    </div>
   </div>
+
 </template>
+
+<script setup >
+
+const darkMode = ref(false)
+
+</script>
---
+++ components/Sidebar/Left/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class='h-screen flex flex-col'>
+
+    <div class="p-2 my-2 rounded-full hover:bg-blue-50 w-min dark:hover:bg-white/20" :class="defaultTransition">
+      <nuxt-link to="/">
+        <div class="w-8 h-8">
+          <LogoTwitter />
+        </div>
+      </nuxt-link>
+    </div>
+
+    <SidebarLeftTab active>
+      <template v-slot:icon>
+        <HomeIcon />
+      </template>
+      <template v-slot:name>
+        Home
+      </template>
+    </SidebarLeftTab>
+  </div>
+</template>
+
+<script setup>
+import { HomeIcon } from "@heroicons/vue/solid";
+const { defaultTransition } = useTailwindConfig()
+
+// const transition = 'transition duration-350 ease-in-out'
+
+
+
+</script>
